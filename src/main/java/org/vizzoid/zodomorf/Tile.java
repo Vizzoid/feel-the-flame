b8c6c25f83efd07c1bffe118995a0e5692f55acc
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-05-12 09:36:01 -0400

    Update Tile.java
    material

--- src/main/java/org/vizzoid/zodomorf/Tile.java
+++ src/main/java/org/vizzoid/zodomorf/Tile.java
@@ -1,10 +1,33 @@
 public class Tile {
     
+    private Planet planet;
     private double temperature;
+    private Material background = Material.EMPTY;
+    private Material material = Material.EMPTY;
+    
+    public Material getBackground() {
+        return this.background;
+    }
+    
+    public Material getMaterial() {
+        return this.material;
+    }
+    
+    public void setBackground(Material material) {
+        this.background = background;
+    }
+    
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
     
     public double transitionTemperature(double temperature, long ticks) {
         double change = temperature > this.temperature ? -0.1 : 0.1;
         return temperature + change;
+    }
+    
+    public void tick(long ticks) {
+        if (background.isEmpty()) planet.transitionTemperature(this.temperature, ticks);
     }
     
 }
