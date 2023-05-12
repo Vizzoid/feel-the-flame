71ac358766918f8407e927bf20dd2fd4bb936661
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-05-12 09:26:13 -0400

    Update Material.java
    air material

--- src/main/java/org/vizzoid/zodomorf/Material.java
+++ src/main/java/org/vizzoid/zodomorf/Material.java
@@ -1,7 +1,15 @@
 public class Material {
+    
+    public static final Material EMPTY = new Material(true);
 
+    private boolean empty;
+    
+    public Material(boolean empty) {
+        this.empty = empty;
+    }
+    
     public boolean isEmpty() {
-        return false;
+        return empty;
     }
     
 }
