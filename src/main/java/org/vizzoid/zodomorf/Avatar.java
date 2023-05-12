bebbd08f11631860c755826be29d9a2a0efdac43
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-05-12 09:14:21 -0400

    Create Avatar.java
    Player character

---
+++ src/main/java/org/vizzoid/zodomorf/Avatar.java
@@ -0,0 +1,30 @@
+public class Avatar {
+
+    private double temperature = 100;
+    private boolean warmingUp = false;
+    private Tile tile;
+  
+    public boolean isOutside() {
+      return this.tile.getBackground().isEmpty();
+    }
+  
+    public boolean isTooCold() {
+        return temperature < 10;
+    }
+    
+    public boolean isWarmingUp() {
+       return warmingUp;
+    }
+  
+    public void tick(long ticks) {
+        if (isOutside()) {
+            newTemperature = tile.transitionTemperature(temperature, ticks);
+            this.temperature = newTemperature
+            this.warmingUp = newTemperature > temperature
+        }
+        if (isTooCold() && !isWarmingUp()) {
+            this.health -= 1;
+        }
+    }
+  
+}
